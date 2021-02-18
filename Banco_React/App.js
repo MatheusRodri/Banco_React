949501295679e40ac9e57d376f8c0a31ee91d3a0
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-18 10:07:41 -0300

    App completo
    No futuro receberá novas atualizações

---
+++ Banco_React/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Index from './src/Components/Index';
+
+export default function App() {
+  return (
+    <View>
+      <Index/>
+    </View>
+  );
+}
+
+
